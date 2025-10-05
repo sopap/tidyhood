9cb23fb766563cfdf7e0b6c51b25262030ba9e39
Franck <franck.kengne@gmail.com>
2025-10-04 23:22:02 -0400

    Adjust logo to optimal size: 48/56/64px for better visibility

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,10 +31,10 @@ export function Header() {
           <Image 
             src="/logo.svg"
             alt="Tidyhood"
-            width={200}
-            height={50}
+            width={300}
+            height={75}
             priority
-            className="h-8 md:h-10 lg:h-12 w-auto"
+            className="h-12 md:h-14 lg:h-16 w-auto"
           />
         </Link>
         <nav className="space-x-4">
