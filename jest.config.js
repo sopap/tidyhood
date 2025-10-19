0afb4072c47f79d9881afbf67515b16c68515a20
Franck <franck.kengne@gmail.com>
2025-10-19 01:25:49 -0400

    Fix test configuration: exclude testHelpers from test runs and add environment variables for pricing tests

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,10 @@ const customJestConfig = {
     '**/__tests__/**/*.[jt]s?(x)',
     '**/?(*.)+(spec|test).[jt]s?(x)',
   ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/utils/',
+  ],
 }
 
 module.exports = createJestConfig(customJestConfig)
--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,11 @@
 import '@testing-library/jest-dom'
 
+// Load environment variables for testing
+process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://gbymheksmnenuranuvjr.supabase.co';
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdieW1oZWtzbW5lbnVyYW51dmpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1OTY1MDksImV4cCI6MjA3NTE3MjUwOX0.SSbPkXH5wHjAz7L6uBT8s4NzfXcwHw4wHDax0BoB2ZA';
+process.env.SUPABASE_SERVICE_ROLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdieW1oZWtzbW5lbnVyYW51dmpyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1OTU5NjUwOSwiZXhwIjoyMDc1MTcyNTA5fQ.7bZJDdaPWu90j3KIoaQCYF_p6GeDC6Ph5jxgbs0G4Ew';
+process.env.NYC_TAX_RATE = '0.08875';
+
 // Mock window.matchMedia
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
